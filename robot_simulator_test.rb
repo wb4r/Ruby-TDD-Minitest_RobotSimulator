3fb3bd9d32bb1f8f7c8e45c2b49a4820d2b57537
willemtoledano <willemtoledano@gmail.com>
2015-10-23 12:40:10 +0100

    Robot and Simulator finish

--- robot_simulator_test.rb
+++ robot_simulator_test.rb
@@ -108,34 +108,31 @@ class RobotTurningTest < MiniTest::Unit::TestCase
   end
 end
 
+
+
 class RobotSimulatorTest < MiniTest::Unit::TestCase
   def simulator
     @simulator ||= Simulator.new
   end
 
   def test_instructions_for_turning_left
-    skip
     assert_equal [:turn_left], simulator.instructions('L')
   end
 
   def test_instructions_for_turning_right
-    skip
     assert_equal [:turn_right], simulator.instructions('R')
   end
 
   def test_instructions_for_advancing
-    skip
     assert_equal [:advance], simulator.instructions('A')
   end
 
   def test_series_of_instructions
-    skip
     commands = [:turn_right, :advance, :advance, :turn_left]
     assert_equal commands, simulator.instructions('RAAL')
   end
 
   def test_instruct_robot
-    skip
     robot = Robot.new
     simulator.place(robot, x: -2, y: 1, direction: :east)
     simulator.evaluate(robot, 'RLAALAL')
@@ -144,7 +141,6 @@ class RobotSimulatorTest < MiniTest::Unit::TestCase
   end
 
   def test_instruct_many_robots
-    skip
     robot1 = Robot.new
     robot2 = Robot.new
     robot3 = Robot.new
--- simulator.rb
+++ simulator.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 class Robot
   attr_reader :compass
-  attr_accessor :facing_to
 
   def initialize
     @compass = [:north, :east, :south, :west]
@@ -15,7 +14,7 @@ class Robot
     if compass.include?(direction) 
       @facing_to = direction
     else
-      raise ArgumentError.new("Waddayamean? Not gonna happen. Out.")
+      raise ArgumentError.new("Waddayamean? Not gonna happen. Robot Out.")
     end
   end
 
@@ -25,17 +24,13 @@ class Robot
 
   def turn_right
     index = @compass.find_index(@facing_to) + 1
-    if @compass[index].nil? 
-      index = 0
-    end
+    index = 0 if @compass[index].nil?
     @facing_to = @compass[index]
   end
 
   def turn_left
     index = @compass.find_index(@facing_to) - 1
-    if @compass[index].nil? 
-      index = 3
-    end
+    index = 3 if @compass[index].nil?
     @facing_to = @compass[index]
   end
 
@@ -54,13 +49,62 @@ class Robot
   def advance
     case @facing_to
     when :north
-      @y_axis =+ 1
+      @y_axis[0] += 1
     when :east
-      @x_axis =+ 1
+      @x_axis[0] += 1
     when :south
-      @y_axis =- 1
+      @y_axis[0] -= 1
     when :west
-      @x_axis =- 1
+      @x_axis[0] -= 1
     end
   end
 end
+
+
+class Simulator
+
+  def initialize
+    @commands = []
+  end
+
+  def instructions(letter_orders)
+   counter = 0
+    while counter < letter_orders.length
+      case letter_orders[counter]
+      when 'L' 
+        @commands << :turn_left
+      when 'R' 
+        @commands << :turn_right
+      when 'A' 
+        @commands << :advance
+      end
+     counter+= 1
+    end
+    @commands
+  end
+
+  def place(robot, coordinates)
+    robot.orient(coordinates[:direction])
+    robot.at(coordinates[:x], coordinates[:y])
+  end
+
+  def evaluate(robot, commands)
+    @simulator = Simulator.new
+    commands = @simulator.instructions(commands)
+
+    counter = 0    
+    while counter < commands.length
+      case commands[counter]
+      when :turn_right
+        robot.turn_right
+      when :turn_left
+        robot.turn_left
+      when :advance
+        robot.advance
+      end
+      counter += 1      
+    end        
+  end
+end
+
+
